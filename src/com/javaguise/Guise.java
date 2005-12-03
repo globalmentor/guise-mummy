596eab621f362700bab947d17e4fc0745a7ab3b6
Garret Wilson <garret@globalmentor.com>
2005-12-03 01:44:01 +0000

    created JavaScript compiler bug workaround for regular expression modifier characters mistakenly interpreted as variable names

--- src/com/javaguise/Guise.java
+++ src/com/javaguise/Guise.java
@@ -29,7 +29,7 @@ public class Guise
 	public final static String PUBLIC_RESOURCE_BASE_PATH="pub/";
 
 	/**The identifier of this build.*/
-	public final static String BUILD_ID="2005-12-01";
+	public final static String BUILD_ID="2005-12-02";
 	
 	/**The singleton instance of Guise.*/
 	private static Guise instance=null;
