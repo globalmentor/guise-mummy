895fe4ff2a17e0bf62cd9ac6afe9d0d3114b39f5
Garret Wilson <garret@globalmentor.com>
2008-08-16 17:21:33 +0000

    Released Marmox 0.3.0.

--- src/com/guiseframework/Guise.java
+++ src/com/guiseframework/Guise.java
@@ -43,7 +43,7 @@ public final class Guise
 	public final static String GUISE_ASSETS_BASE_KEY="assets/";
 
 	/**The identifier of this build.*/
-	public final static String BUILD_ID="2008-07-27";
+	public final static String BUILD_ID="2008-08-10";
 
 		//Guise ontology
 	
