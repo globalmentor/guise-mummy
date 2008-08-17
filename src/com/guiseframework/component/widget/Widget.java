61656830f87d8ac14abd9dc5cea0fcfc3abdf424
Garret Wilson <garret@globalmentor.com>
2008-08-17 22:06:28 +0000

    Improved Guise widget API to have knowledge of base URI.

--- src/com/guiseframework/component/widget/Widget.java
+++ src/com/guiseframework/component/widget/Widget.java
@@ -27,7 +27,7 @@ import com.guiseframework.component.Component;
 
 /**A component that can be embedded in certain content and can be edited within a widget editing environment.
 Widgets must provide a default constructor.
-A widget may also provide a URI constructor that allows specification of the base URI of the definition of the widget, such as a template.
+A widget may also provide a two-URI constructor that allows specification of the base URI where the widget is embedded, as well as the URI of the definition of the widget, such as a template, either of which may be <code>null</code>.
 @author Garret Wilson
 */
 public interface Widget extends Component
@@ -35,6 +35,11 @@ public interface Widget extends Component
 
 	/**The MIME type of a Guise widget.*/
 	public final static ContentType WIDGET_CONTENT_TYPE=getContentTypeInstance(APPLICATION_PRIMARY_TYPE, SUBTYPE_EXTENSION_PREFIX+"guise-widget");
+
+	/**Returns the base URI where the widget is embedded.
+	@return The base URI where the widget is embedded, or <code>null</code> if the base URI is not known.
+	*/
+	public URI getBaseURI();
 
 	/**Returns the base URI of the definition of the widget.
 	This may be the URI of a template resource, allowing the true base URI of the widget to vary based upon the instance resource with which the resource is used.
