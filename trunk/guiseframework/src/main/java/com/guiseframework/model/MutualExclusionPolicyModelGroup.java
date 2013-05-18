5bfe8adf02b375b7db30b2bd45da203e38f2708c
Garret Wilson <garret@globalmentor.com>
2013-05-18 22:17:41 +0000

    MARMOX-5: Improved declarative MAQRO activity presentation, including question numbering.

--- trunk/guiseframework/src/main/java/com/guiseframework/model/MutualExclusionPolicyModelGroup.java
+++ trunk/guiseframework/src/main/java/com/guiseframework/model/MutualExclusionPolicyModelGroup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2005-2008 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ * Copyright © 2005-2013 GlobalMentor, Inc. <http://www.globalmentor.com/>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,49 +21,68 @@ import java.util.*;
 
 import com.globalmentor.beans.GenericPropertyChangeEvent;
 
-/**A group that ensures that only one boolean model in the group is set to <code>true</code> at the same time.
-This class is declared final because it represents a particular defined semantics, no more and no less.
-This allows controllers to make assumptions about models with only this listener, and offload mutual exclusion to client that have this capability built in.
-The current implementation will only update the selected model for models that are already a part of the group.
-@author Garret Wilson.
-*/
-public final class MutualExclusionPolicyModelGroup extends ValuePolicyModelGroup<Boolean>	//TODO improve class to update the selected model when models are added or removed
+/**
+ * A group that ensures that only one boolean model in the group is set to <code>true</code> at the same time.
+ * <p>
+ * This class is declared final because it represents a particular defined semantics, no more and no less. This allows controllers to make assumptions about
+ * models with only this listener, and offload mutual exclusion to client that have this capability built in.
+ * </p>
+ * <p>
+ * The current implementation will only update the selected model for models that are already a part of the group.
+ * </p>
+ * @author Garret Wilson.
+ */
+public final class MutualExclusionPolicyModelGroup extends ValuePolicyModelGroup<Boolean> //TODO improve class to update the selected model when models are added or removed
 {
 
-	/**Model constructor.
-	@param models Zero or more models with which to initially place in the group.
-	@exception NullPointerException if one of the models is <code>null</code>.
-	*/
-	public MutualExclusionPolicyModelGroup(final ValueModel<Boolean>... models)
+	/**
+	 * Default constructor. Provided to prevent array of generic types warning.
+	 */
+	@SuppressWarnings("unchecked")
+	public MutualExclusionPolicyModelGroup()
 	{
-		super(models);	//construct the parent class
+		this(new ValueModel[0]);
 	}
 
-	/**The currently selected model.*/
-	private ValueModel<Boolean> selectedModel=null;
+	/**
+	 * Model constructor.
+	 * @param models Zero or more models with which to initially place in the group.
+	 * @exception NullPointerException if one of the models is <code>null</code>.
+	 */
+	public MutualExclusionPolicyModelGroup(final ValueModel<Boolean>... models)
+	{
+		super(models); //construct the parent class
+	}
 
-		/**@return The currently selected model.*/
-		public ValueModel<Boolean> getSelectedModel() {return selectedModel;}
+	/** The currently selected model. */
+	private ValueModel<Boolean> selectedModel = null;
 
-	/**Called when the boolean model value is changed.
-	@param propertyChangeEvent An event object describing the event source, the property that has changed, and its old and new values.
-	*/
+	/** @return The currently selected model. */
+	public ValueModel<Boolean> getSelectedModel()
+	{
+		return selectedModel;
+	}
+
+	/**
+	 * Called when the boolean model value is changed.
+	 * @param propertyChangeEvent An event object describing the event source, the property that has changed, and its old and new values.
+	 */
 	public void propertyChange(final GenericPropertyChangeEvent<Boolean> propertyChangeEvent)
 	{
-		if(Boolean.TRUE.equals(propertyChangeEvent.getNewValue()))	//if this model is changing to true, change the other models to false
+		if(Boolean.TRUE.equals(propertyChangeEvent.getNewValue())) //if this model is changing to true, change the other models to false
 		{
-			final ValueModel<Boolean> source=(ValueModel<Boolean>)propertyChangeEvent.getSource();	//see which model changed TODO verify, improve cast
-			selectedModel=source;	//update the selected model
-			final Set<ValueModel<Boolean>> modelSet=getModelSet();	//get the set of models in the group
-			for(final ValueModel<Boolean> valueModel:modelSet)	//for each model in the group
+			final ValueModel<Boolean> source = (ValueModel<Boolean>)propertyChangeEvent.getSource(); //see which model changed TODO verify, improve cast
+			selectedModel = source; //update the selected model
+			final Set<ValueModel<Boolean>> modelSet = getModelSet(); //get the set of models in the group
+			for(final ValueModel<Boolean> valueModel : modelSet) //for each model in the group
 			{
-				if(valueModel!=source)	//if this is not the source (the source model should keep the value of true)
+				if(valueModel != source) //if this is not the source (the source model should keep the value of true)
 				{
 					try
 					{
-						valueModel.setValue(Boolean.FALSE);	//set the values of the other value models to false (which will fire other events, but will be ignored by this class because the value is false)
+						valueModel.setValue(Boolean.FALSE); //set the values of the other value models to false (which will fire other events, but will be ignored by this class because the value is false)
 					}
-					catch(final PropertyVetoException propertyVetoException)	//if the change was vetoed, ignore the exception
+					catch(final PropertyVetoException propertyVetoException) //if the change was vetoed, ignore the exception
 					{
 					}
 				}
