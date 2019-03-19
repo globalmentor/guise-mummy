038bf6e782c077886fcfcdad99f58d9eaa9f23dc
Garret Wilson <garret@globalmentor.com>
2019-03-19 10:31:57 -0700

    GUISE-24: Fixed Javadoc problems from previously commenting out code.

--- framework/src/main/java/io/guise/framework/theme/Theme.java
+++ framework/src/main/java/io/guise/framework/theme/Theme.java
@@ -110,42 +110,44 @@ public class Theme extends ArrayList<Rule> /*TODO delete legacy URF extends URFL
 		return combinedRuleSet != null ? combinedRuleSet : Collections.<Rule>emptySet(); //return the combined set of rules we've found (Java won't allow emptySet() to be used in this context, but a warning here is better than alternate, less-efficient methods)
 	}
 
-	/** Default constructor. */
+	//TODO delete legacy URF
+	//	/** Default constructor. */
 	/*TODO delete legacy URF
 	public Theme() {
 		this((URI)null); //construct the class with no reference URI
 	}
 	*/
 
-	/**
-	 * Reference URI constructor.
-	 * @param referenceURI The reference URI for the new resource.
-	 */
+	//TODO delete legacy URF
+	//	/**
+	//	 * Reference URI constructor.
+	//	 * @param referenceURI The reference URI for the new resource.
+	//	 */
 	/*TODO delete legacy URF
 	public Theme(final URI referenceURI) {
 		super(referenceURI, createResourceURI(THEME_NAMESPACE_URI, getLocalName(Theme.class))); //construct the parent class, using a type based upon the name of this class
 	}
 	 */
-
-	/**
-	 * Collection constructor with no URI. The elements of the specified collection will be added to this list in the order they are returned by the collection's
-	 * iterator.
-	 * @param collection The collection whose elements are to be placed into this list.
-	 * @throws NullPointerException if the specified collection is <code>null</code>.
-	 */
+	//TODO delete legacy URF
+	//	/**
+	//	 * Collection constructor with no URI. The elements of the specified collection will be added to this list in the order they are returned by the collection's
+	//	 * iterator.
+	//	 * @param collection The collection whose elements are to be placed into this list.
+	//	 * @throws NullPointerException if the specified collection is <code>null</code>.
+	//	 */
 	/*TODO delete legacy URF
 	public Theme(final Collection<? extends Rule> collection) {
 		this(null, collection); //construct the class with no URI
 	}
 	 */
-
-	/**
-	 * URI and collection constructor. The elements of the specified collection will be added to this list in the order they are returned by the collection's
-	 * iterator.
-	 * @param uri The URI for the resource, or <code>null</code> if the resource should have no URI.
-	 * @param collection The collection whose elements are to be placed into this list.
-	 * @throws NullPointerException if the specified collection is <code>null</code>.
-	 */
+	//TODO delete legacy URF
+	//	/**
+	//	 * URI and collection constructor. The elements of the specified collection will be added to this list in the order they are returned by the collection's
+	//	 * iterator.
+	//	 * @param uri The URI for the resource, or <code>null</code> if the resource should have no URI.
+	//	 * @param collection The collection whose elements are to be placed into this list.
+	//	 * @throws NullPointerException if the specified collection is <code>null</code>.
+	//	 */
 	/*TODO delete legacy URF
 	public Theme(final URI uri, final Collection<? extends Rule> collection) {
 		this(uri); //construct the class with the URI
