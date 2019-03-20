cf98e66ee47a1a85a55cc2764f7d58efaccea87b
Garret Wilson <garret@globalmentor.com>
2019-03-20 10:23:32 -0700

    GUISE-29: Created logic outline for finding a navigation list to regenerate.

--- mummy/src/main/java/io/guise/mummy/AbstractPageMummifier.java
+++ mummy/src/main/java/io/guise/mummy/AbstractPageMummifier.java
@@ -16,15 +16,18 @@
 
 package io.guise.mummy;
 
+import static com.globalmentor.html.spec.HTML.*;
 import static com.globalmentor.io.Paths.*;
+import static com.globalmentor.xml.XML.*;
 import static java.nio.file.Files.*;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.util.List;
 
 import javax.annotation.*;
 
-import org.w3c.dom.Document;
+import org.w3c.dom.*;
 
 import com.globalmentor.html.HtmlSerializer;
 
@@ -51,14 +54,16 @@ public abstract class AbstractPageMummifier extends AbstractSourcePathMummifier 
 	@Override
 	public void mummify(final MummyContext context, final Artifact contextArtifact, final Artifact artifact) throws IOException {
 
-		final Document document = loadSourceDocument(artifact.getSourcePath());
+		final Document sourceDocument = loadSourceDocument(artifact.getSourcePath());
+		System.out.println("loaded source document: " + artifact.getSourcePath());
 
-		System.out.println("parsed document: " + artifact.getSourcePath());
+		final Document processedDocument = processDocument(sourceDocument);
+
 		try (final OutputStream outputStream = new BufferedOutputStream(newOutputStream(artifact.getTargetPath()))) {
 			final HtmlSerializer htmlSerializer = new HtmlSerializer(true);
-			htmlSerializer.serialize(document, outputStream);
+			htmlSerializer.serialize(processedDocument, outputStream);
 		}
-		System.out.println("generated document: " + artifact.getTargetPath());
+		System.out.println("generated output document: " + artifact.getTargetPath());
 
 	}
 
@@ -75,5 +80,97 @@ public abstract class AbstractPageMummifier extends AbstractSourcePathMummifier 
 	 * @throws IOException if there is an error loading and/or converting the source file contents.
 	 */
 	protected abstract Document loadSourceDocument(@Nonnull final Path sourceFile) throws IOException;
+
+	/**
+	 * Processes a source document before it is converted to an output document.
+	 * @param sourceDocument The source document to process.
+	 * @return The processed document ready, which may or may not be the same document supplied as input.
+	 * @throws IOException if there is an error processing the document.
+	 */
+	protected Document processDocument(@Nonnull final Document sourceDocument) throws IOException {
+		processChildElements(sourceDocument.getDocumentElement()); //process the root children, because the root can't be replaced
+		return sourceDocument;
+	}
+
+	/**
+	 * Processes a source document element.
+	 * <p>
+	 * The element is replaced with the returned elements. If only the same element is returned, no replacement is made. If no element is returned, the source
+	 * element is removed.
+	 * </p>
+	 * @param sourceElement The source element to process.
+	 * @return The processed element(s), if any, to replace the source element.
+	 * @throws IOException if there is an error processing the element.
+	 */
+	protected List<Element> processElement(@Nonnull final Element sourceElement) throws IOException {
+
+		//TODO transfer to some system of pluggable element processing strategies
+
+		if(XHTML_NAMESPACE_URI.toString().equals(sourceElement.getNamespaceURI())) {
+			if(ELEMENT_OL.equals(sourceElement.getLocalName()) || ELEMENT_UL.equals(sourceElement.getLocalName())) { //<ol> or <ul>
+
+				boolean inMummyRegenerateNav = false; //TODO create XML utility method hasParent()
+				Node parentNode = sourceElement.getParentNode();
+				while(!inMummyRegenerateNav && parentNode instanceof Element) {
+					final Element parentElement = (Element)parentNode;
+					if(XHTML_NAMESPACE_URI.toString().equals(parentNode.getNamespaceURI()) && ELEMENT_NAV.equals(parentNode.getLocalName())) { //<nav>
+						//TODO create an Optional findDefinedAttributeNS() method, with true URI variation
+						if("regenerate".equals(getDefinedAttributeNS(parentElement, "https://guise.io/name/mummy/", "regenerate"))) { //TODO use constants
+							inMummyRegenerateNav = true;
+						}
+					}
+					parentNode = parentNode.getParentNode();
+				}
+
+				if(inMummyRegenerateNav) {
+					return regenerateNavigationList(sourceElement);
+				}
+			}
+		}
+
+		processChildElements(sourceElement);
+
+		return List.of(sourceElement);
+	}
+
+	/**
+	 * Processes child elements of an existing element.
+	 * @implNote This implementation does not yet allow returning different nodes than the one being processed.
+	 * @param sourceElement The source element the children of which to process.
+	 * @throws IOException if there is an error processing the child elements.
+	 */
+	protected void processChildElements(@Nonnull final Element sourceElement) throws IOException {
+		final NodeList childNodes = sourceElement.getChildNodes();
+		for(int childNodeIndex = 0; childNodeIndex < childNodes.getLength();) { //advance the index manually as needed
+			final Node childNode = childNodes.item(childNodeIndex);
+			if(!(childNode instanceof Element)) { //skip non-elements
+				childNodeIndex++;
+				continue;
+			}
+			final Element childElement = (Element)childNode;
+			final List<Element> processedElements = processElement(childElement);
+			final int processedElementCount = processedElements.size();
+			childNodeIndex += processedElementCount; //manually advance the index based upon the replacement nodes
+			if(processedElementCount == 1 && processedElements.get(0) == childElement) { //if no structural changes were requested
+				continue;
+			}
+			throw new UnsupportedOperationException("Structural changes not yet supported when processing individual child elements.");
+		}
+	}
+
+	/**
+	 * Processes a source document element.
+	 * <p>
+	 * The element is replaced with the returned elements. If only the same element is returned, no replacement is made. If no element is returned, the source
+	 * element is removed.
+	 * </p>
+	 * @param sourceElement The source element to process.
+	 * @return The processed element(s), if any, to replace the source element.
+	 * @throws IOException if there is an error processing the element.
+	 */
+	protected List<Element> regenerateNavigationList(@Nonnull final Element navigationListElement) throws IOException {
+		System.out.println("-----> We should regenerate a navigation list!");
+		return List.of(navigationListElement);
+	}
 
 }
