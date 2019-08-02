c204412801863ab0c4ec5342fe7c964bc6c85ff8
Garret Wilson <garret@globalmentor.com>
2019-08-02 07:35:31 -0700

    GUISE-49: Added basic Markdown document support.

---
+++ mummy/src/main/java/io/guise/mummy/MarkdownPageMummifier.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright © 2019 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.guise.mummy;
+
+import static java.nio.charset.StandardCharsets.*;
+
+import java.io.*;
+import java.util.Set;
+
+import javax.xml.parsers.*;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import com.globalmentor.io.BOMInputStreamReader;
+import com.globalmentor.text.StringTemplate;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+
+/**
+ * Mummifier for Markdown documents.
+ * @implSpec This implementation defaults to UTF-8, producing an error if the encoding isn't valid. Other encodings are supported with the appropriate BOM.
+ * @author Garret Wilson
+ */
+public class MarkdownPageMummifier extends AbstractPageMummifier {
+
+	/**
+	 * The template for wrapping an XHTML document around the generated HTML. It has the following parameters:
+	 * <ol>
+	 * <li>Page {@code <title>} content.</li>
+	 * <li>Page {@code <body>} content.</li>
+	 * </ol>
+	 */
+	private static final StringTemplate XHTML_TEMPLATE = StringTemplate.builder() //@formatter:off
+					.text("<?xml version=\"1.0\" encoding=\"UTF-8\"?>").newline()
+					.text("<!DOCTYPE html>").newline()
+					.text("<html xmlns=\"http://www.w3.org/1999/xhtml\">").newline() 
+					.text("<head>").newline() 
+					.text("<title>").parameter(StringTemplate.STRING_PARAMETER).text("</title>").newline() 
+					.text("</head>").newline()
+					.text("<body>").newline()
+					.parameter(StringTemplate.STRING_PARAMETER)
+					.text("</body>")
+					.text("</html>").newline().build();	//@formatter:on
+
+	/**
+	 * {@inheritDoc}
+	 * @implNote The supported extensions follow the two given in <a href="https://tools.ietf.org/html/rfc7763">RFC 7763</a> so as to promote consistency, namely
+	 *           <code>md</code> and <code>markdown</code>, even though there are <a href="https://superuser.com/a/285878/954883">many variations</a> in the wild.
+	 */
+	@Override
+	public Set<String> getSupportedFilenameExtensions() {
+		return Set.of("md", "markdown");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @implSpec This version loads a document in Markdown format.
+	 */
+	@Override
+	public Document loadSourceDocument(final MummyContext context, final InputStream inputStream) throws IOException, DOMException {
+		//create a Reader to detect the BOM and to throw errors if the encoding is invalid
+		@SuppressWarnings("resource") //we don't manage the underlying input stream
+		final BOMInputStreamReader bomInputStreamReader = new BOMInputStreamReader(new BufferedInputStream(inputStream)); //TODO create utility to ensure mark supported
+		final StringBuilder stringBuilder = new StringBuilder();
+		final char[] buffer = new char[64 * 1024];
+		int charsRead;
+		while((charsRead = bomInputStreamReader.read(buffer)) != -1) { //TODO create utility to read all from a reader
+			stringBuilder.append(buffer, 0, charsRead);
+		}
+		final Parser parser = Parser.builder().build();
+		com.vladsch.flexmark.util.ast.Document document = parser.parse(stringBuilder.toString());
+		final HtmlRenderer renderer = HtmlRenderer.builder().build();
+		final String htmlBodyContent = renderer.render(document);
+		final String xhtmlDocumentString = XHTML_TEMPLATE.apply("", htmlBodyContent); //TODO title
+		final DocumentBuilder documentBuilder = context.newPageDocumentBuilder();
+		try {
+			return documentBuilder.parse(new ByteArrayInputStream(xhtmlDocumentString.getBytes(UTF_8)));
+		} catch(final SAXException saxException) {
+			throw new IOException(saxException);
+		}
+	}
+}
---
+++ mummy/src/test/java/io/guise/mummy/MarkdownPageMummifierTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright © 2019 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.guise.mummy;
+
+import static com.github.npathai.hamcrestopt.OptionalMatchers.*;
+import static com.globalmentor.html.HtmlDom.*;
+import static com.globalmentor.html.spec.HTML.*;
+import static com.globalmentor.xml.XmlDom.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.*;
+
+import org.junit.jupiter.api.*;
+import org.w3c.dom.*;
+
+import io.confound.config.Configuration;
+
+/**
+ * Tests of {@link MarkdownPageMummifier}.
+ * @author Garret Wilson
+ */
+public class MarkdownPageMummifierTest {
+
+	public static final String SIMPLE_MARKDOWN_RESOURCE_NAME = "simple.md";
+
+	private MummyContext mummyContext;
+
+	@BeforeEach
+	protected void setupContext() {
+		mummyContext = new BaseMummyContext() { //TODO move to separate mock context
+
+			@Override
+			public Path getSiteTargetDirectory() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public Path getSiteSourceDirectory() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public Path getSiteDescriptionTargetDirectory() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public Configuration getSiteConfiguration() {
+				return Configuration.empty();
+			}
+
+			@Override
+			public SourcePathMummifier getDefaultSourceFileMummifier() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public SourcePathMummifier getDefaultSourceDirectoryMummifier() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public Optional<SourcePathMummifier> findRegisteredMummifierForSourceFile(Path sourceFile) {
+				return Optional.empty();
+			}
+
+			@Override
+			public Optional<SourcePathMummifier> findRegisteredMummifierForSourceDirectory(Path sourceDirectory) {
+				return Optional.empty();
+			}
+		};
+	}
+
+	/**
+	 * @see MarkdownPageMummifier#loadSourceDocument(MummyContext, InputStream)
+	 * @see #SIMPLE_MARKDOWN_RESOURCE_NAME
+	 */
+	@Test
+	public void testSimpleMarkdown() throws IOException {
+		final MarkdownPageMummifier mummifier = new MarkdownPageMummifier();
+		final Document document;
+		try (final InputStream inputStream = getClass().getResourceAsStream("simple.md")) {
+			document = mummifier.loadSourceDocument(mummyContext, inputStream);
+		}
+		assertThat(findTitle(document), isPresentAndIs(""));
+		final Node body = findHtmlBodyElement(document).orElseThrow(AssertionError::new);
+		final List<Element> bodyElements = getChildElements(body);
+		assertThat(bodyElements, hasSize(2));
+		final Element h1 = bodyElements.get(0);
+		assertThat(h1.getLocalName(), is(ELEMENT_H1));
+		assertThat(h1.getNamespaceURI(), is(XHTML_NAMESPACE_URI_STRING));
+		assertThat(h1.getTextContent(), is("Heading"));
+		final Element p = bodyElements.get(1);
+		assertThat(p.getLocalName(), is(ELEMENT_P));
+		assertThat(p.getNamespaceURI(), is(XHTML_NAMESPACE_URI_STRING));
+		assertThat(p.getTextContent(), is("Body text."));
+	}
+
+}
