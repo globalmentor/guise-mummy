c84a020bebf3e0479de8b434e7ac9ee5754939db
Garret Wilson <garret@globalmentor.com>
2019-10-01 10:48:31 -0300

    GUISE-82: Added more standard DNS resource record types with IDs.

--- mummy/src/main/java/io/guise/mummy/deploy/Dns.java
+++ mummy/src/main/java/io/guise/mummy/deploy/Dns.java
@@ -31,9 +31,113 @@ public interface Dns {
 	/**
 	 * Common, known resource record types.
 	 * @apiNote This list is not exhaustive, but provides merely a convenience, type-safe approach for indicating common types.
+	 * @see <a href="https://en.wikipedia.org/wiki/List_of_DNS_record_types">List of DNS record types</a>
+	 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+	 * @see <a href="https://tools.ietf.org/html/rfc2308">RFC 2308</a>
 	 */
 	public static enum ResourceRecordType {
-		A, CNAME, TXT; //TODO add others
+
+		/**
+		 * A host address.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		A(1),
+		/**
+		 * The canonical name for an alias
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		CNAME(5),
+		/**
+		 * Mailbox or mail list information.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		HINFO(14),
+		/**
+		 * A mailbox domain name.
+		 * @apiNote Experimental.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		MB(7),
+		/**
+		 * A mail destination.
+		 * @deprecated Obsolete; use {@link #MX}.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		@Deprecated
+		MD(3),
+		/**
+		 * A mail forwarder.
+		 * @deprecated Obsolete; use {@link #MX}.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		@Deprecated
+		MF(3),
+		/**
+		 * A mail group member.
+		 * @apiNote Experimental.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		MG(8),
+		/**
+		 * A mail rename domain name.
+		 * @apiNote Experimental.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		MR(9),
+		/**
+		 * Mail exchange.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 * @see <a href="https://tools.ietf.org/html/rfc7505">RFC 7505</a>
+		 */
+		MX(15),
+		/**
+		 * An authoritative name server.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		NS(2),
+		/**
+		 * A null resource record.
+		 * @apiNote Experimental.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		NULL(10),
+		/**
+		 * A domain name pointer.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		PTR(12),
+		/**
+		 * Marks the start of a zone of authority.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 * @see <a href="https://tools.ietf.org/html/rfc2308">RFC 2308</a>
+		 */
+		SOA(6),
+		/**
+		 * Text strings.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		TXT(16),
+		/**
+		 * A well known service description.
+		 * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+		 */
+		WKS(11);
+
+		private final int id;
+
+		/** @return The type ID. */
+		public int getId() {
+			return id;
+		}
+
+		/**
+		 * Constructor.
+		 * @param id The type ID.
+		 */
+		private ResourceRecordType(final int id) {
+			this.id = id;
+		}
+
 	}
 
 	/**
