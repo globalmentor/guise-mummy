0f2f42c6f4c7c47e655aa0b4d6fbf204cc228bda
Garret Wilson <garret@globalmentor.com>
2021-01-16 14:50:17 -0800

    GUISE-175: Implemented base interpolation logic for Guise Mesh.

---
+++ mesh/src/main/java/io/guise/mesh/MeshInterpolationException.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2020 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.guise.mesh;
+
+import javax.annotation.*;
+
+/**
+ * An meshing exception indicating a problem with the interpolation syntax (e.g. a missing ending brace) of text being interpolated.
+ * @author Garret Wilson
+ */
+public class MeshInterpolationException extends MeshException {
+
+	private static final long serialVersionUID = 1L;
+
+	/** No-argument constructor. */
+	public MeshInterpolationException() {
+		this((String)null);
+	}
+
+	/**
+	 * Message constructor.
+	 * @param message An explanation of the error, or <code>null</code> if no message should be used.
+	 */
+	public MeshInterpolationException(@Nullable final String message) {
+		this(message, null);
+	}
+
+	/**
+	 * Cause constructor. The message of the cause will be used if available.
+	 * @param cause The cause of the error or <code>null</code> if the cause is nonexistent or unknown.
+	 */
+	public MeshInterpolationException(@Nullable final Throwable cause) {
+		this(cause == null ? null : cause.toString(), cause);
+	}
+
+	/**
+	 * Message and cause constructor.
+	 * @param message An explanation of the error, or <code>null</code> if a no message should be used.
+	 * @param cause The cause of the error or <code>null</code> if the cause is nonexistent or unknown.
+	 */
+	public MeshInterpolationException(@Nullable final String message, @Nullable final Throwable cause) {
+		super(message, cause);
+	}
+
+}
---
+++ mesh/src/test/java/io/guise/mesh/DefaultMeshInterpolatorTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2020 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.guise.mesh;
+
+import static com.github.npathai.hamcrestopt.OptionalMatchers.*;
+import static io.guise.mesh.DefaultMeshInterpolator.findInterpolation;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.globalmentor.text.ASCII;
+
+public class DefaultMeshInterpolatorTest {
+
+	/**
+	 * Tests the low-level general interpolation implementation that uses an expression evaluation function.
+	 * @see DefaultMeshInterpolator#findInterpolation(CharSequence, String, String, java.util.function.Function)
+	 */
+	@Test
+	void testBaseFindInterpolationMethod() {
+		assertThat("No expressions in empty string.", findInterpolation("", "^{", "}", ASCII::toUpperCase).map(Object::toString), isEmpty());
+		assertThat("No expressions to find.", findInterpolation("foobar", "^{", "}", ASCII::toUpperCase).map(Object::toString), isEmpty());
+		assertThat("False signal character.", findInterpolation("foo^bar", "^{", "}", ASCII::toUpperCase).map(Object::toString), isEmpty());
+		assertThat("False signal character at end of string.", findInterpolation("foobar^", "^{", "}", ASCII::toUpperCase).map(Object::toString), isEmpty());
+		assertThat("False left delimiter.", findInterpolation("foo{bar", "^{", "}", ASCII::toUpperCase).map(Object::toString), isEmpty());
+		assertThat("False right delimiter.", findInterpolation("foo}bar", "^{", "}", ASCII::toUpperCase).map(Object::toString), isEmpty());
+		assertThat("Entire string is expression", findInterpolation("^{foo}", "^{", "}", ASCII::toUpperCase).map(Object::toString), isPresentAndIs("FOO"));
+		assertThat("Expression at start of string.", findInterpolation("^{foo}bar", "^{", "}", ASCII::toUpperCase).map(Object::toString), isPresentAndIs("FOObar"));
+		assertThat("Expression in middle of string.", findInterpolation("fo^{oba}r", "^{", "}", ASCII::toUpperCase).map(Object::toString),
+				isPresentAndIs("foOBAr"));
+		assertThat("Expression at end of string.", findInterpolation("foo^{bar}", "^{", "}", ASCII::toUpperCase).map(Object::toString), isPresentAndIs("fooBAR"));
+		assertThat("Two subsequent expressions.", findInterpolation("^{foo}^{bar}", "^{", "}", ASCII::toUpperCase).map(Object::toString), isPresentAndIs("FOOBAR"));
+		assertThat("Two separated expressions.", findInterpolation("f^{oo}b^{a}r", "^{", "}", ASCII::toUpperCase).map(Object::toString), isPresentAndIs("fOObAr"));
+		assertThat("Three separated expressions.", findInterpolation("^{f}oo^{b}a^{r}", "^{", "}", ASCII::toUpperCase).map(Object::toString),
+				isPresentAndIs("FooBaR"));
+		assertThat("Expression after a false signal character.", findInterpolation("f^o^{oba}r", "^{", "}", ASCII::toUpperCase).map(Object::toString),
+				isPresentAndIs("f^oOBAr"));
+		assertThat("Two expressions separated by a false signal character.",
+				findInterpolation("^{fo}o^b^{ar}", "^{", "}", ASCII::toUpperCase).map(Object::toString), isPresentAndIs("FOo^bAR"));
+		assertThat("Two expressions separated by two false signal characters.",
+				findInterpolation("a^{br}a^ca^d^{abr}a", "^{", "}", ASCII::toUpperCase).map(Object::toString), isPresentAndIs("aBRa^ca^dABRa"));
+		assertThrows(MeshInterpolationException.class, () -> findInterpolation("^{foobar", "^{", "}", ASCII::toUpperCase),
+				"Incomplete expression at start of string.");
+		assertThrows(MeshInterpolationException.class, () -> findInterpolation("foo^{bar", "^{", "}", ASCII::toUpperCase),
+				"Incomplete expression in middle of string.");
+		assertThrows(MeshInterpolationException.class, () -> findInterpolation("foobar^{", "^{", "}", ASCII::toUpperCase),
+				"Incomplete expression at end of string.");
+	}
+
+}
